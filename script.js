45e32a3b9b50a6c3398a61bb00df7f05651b881c
elinlow <elin.andersson.lowenadler@gmail.com>
2021-12-15 22:06:22 +0100

    finished assignment

--- script.js
+++ script.js
@@ -1,32 +1,72 @@
-// BANK ACCOUNT
+const account = {
+  accountName: "Elin's bank account",
+  balance: 100,
+  getBalance: function () {
+    return this.balance;
+  },
 
-// Start by creating the object and the properties. You can leave the functions empty for now if you want.
-// You'll find all the information about what properties the object should have in the readme file.
+  deposit: function () {
+    const depositAmount = parseFloat(prompt("How much would you like to deposit?"));
+    if (!isNaN(depositAmount) && depositAmount > 0) {
+      this.balance = this.getBalance() + depositAmount;
+    } else { 
+      this.accountError("Please enter a valid sum to deposit");
+      this.deposit();
+    }
+  },
 
-// In the deposit and withdrawal function use this code to show the prompt
-parseFloat(prompt("Put some proper message here"));
-// You need to change the message of course. You should also do some research why parseFloat() is used here
-// answer by making a comment in the code.
-// Be aware of that you need to add some code to this so you can display the actual input.
+  withdrawal: function () {
+    const withdrawalAmount = parseInt(prompt("How much would you like to withdraw?"));
+    if (!isNaN(withdrawalAmount) && withdrawalAmount > 0 && this.getBalance() > withdrawalAmount) {
+      this.balance = this.getBalance() - withdrawalAmount;
+    } else if (this.getBalance() < withdrawalAmount) {
+      this.accountError("You do not have enough money");
+      this.withdrawal();
+    } else { 
+      this.accountError("Please enter a valid sum to withdraw");
+      this.withdrawal();
+    } 
+  },
 
-// In the deposit and withdrawal function you also need to handle som potential errors.
-// To handle one of the potential error you can use this piece of code
-isNaN(variableName);
-// you need to change the variableName to your actual variable.
-// you also need to check some other conditions here and use a logical operator.
-// try to say the sentece out loud or think it:
-// "A user should not be able to put 0 or negative numbers or empty string or a type that is not a number"
-// Do you know what operator you should use?
+  getAccountName: function () {
+    alert(this.accountName);
+  },
 
-// In the accountError and accountExit function you can use this to show a message to the user:
-alert("your code here and maybe properties");
-// of course you need to put a proper message and you might need to display some of the properties.
+  accountError: (errorMessage) => {
+    alert(errorMessage);
+  },
 
-// STARTER FUNCTION
-function atm() {
-  let choice = parseInt(prompt("Display menu choices here"));
-  //prompt user for choice.
-
-  // you can use the variable choice for your switch or if/else statement
-  // so choice will hold the value of the user input.
+  exitAccount: function () {
+    alert("Goodbye");
+    close();
+  },
 }
+
+function atm() {
+  let choice = parseInt(prompt("Select a choice. 1) See balance, 2) Make a deposit, 3) Make a withdrawal, 4) Get account name, 5) Exit"));
+  
+  //prompt user for choice.
+  switch (choice) {
+    case 1: 
+      parseInt(prompt("This is your balance: ", account.getBalance()));
+      this.atm();
+      break;
+    case 2:
+      account.deposit();
+      this.atm();
+      break;
+    case 3:
+      account.withdrawal();
+      this.atm();
+      break;
+    case 4:
+      account.getAccountName();
+      this.atm();
+      break;
+    case 5:
+      account.exitAccount();
+      break;
+  }
+}
+
+this.atm();
